cc840e199dc5a6e7e3e38dde60623be3c3c74cf8
Geo-Jagaimo <salamanderezo@gmail.com>
2026-05-15 08:21:20 +0900

    build: package as macOS

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+    QCoreApplication::setOrganizationName("example");
+    QCoreApplication::setApplicationName("hobo-gis");
+    QCoreApplication::setApplicationVersion("1.0");
+
     MainWindow window;
     window.show();
 
